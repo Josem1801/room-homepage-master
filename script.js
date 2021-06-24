b61e555efd4a039cd110f92669c8bd45edde6536
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2021-06-24 13:32:36 -0500

    hamburger fixed

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const left = document.getElementById('btn-left')
 const right = document.getElementById('btn-right')
 const carrusel = document.getElementById('carrusel')
-
 const carruselText = document.getElementById('carrusel-text')
-
-window.addEventListener('change', () => {
-    console.log("hola")
-})
-
+const hamburger = document.querySelector(".header__hamburger")
+const nav = document.querySelector(".header__nav")
 let move = 0
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('header__nav-active')
+    hamburger.classList.toggle('header__hamburger-active')
+})
 
 left.addEventListener('click', () => {
     move < 0 
